9cc1e75765cf573273cc610f88c39d50b491f95c
Parthendo <parthendo@geu.ac.in>
2018-03-02 18:35:02 +0530

    markus__

---
+++ Templates/rand.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+//--------------------------
+/* "Beautiful codes are better than the shorter ones"
+                                      - read from triveni
+    Name: Parth Trehan (markus__)
+    Date: 21.01.18
+*/
+//--------------------------
+#define sd(x)            scanf("%d",&x)
+#define sd2(x,y)         scanf("%d%d",&x,&y)
+#define sd3(x,y,z)       scanf("%d%d%d",&x,&y,&z)
+#define sll(x)           scanf("%lld",&x)
+#define ms(array,value)  memset(array,value,sizeof(array))
+#define iterate(it, s)   for(auto it=s.begin(); it!=s.end(); it++)
+#define bcnt             __builtin_popcount
+#define pb(x)            push_back(x)
+#define sc               scanf
+#define pf               printf
+#define F                first
+#define S                second
+#define io1              ios_base::sync_with_stdio(0)
+#define io2              cin.tie(NULL)
+//--------------------------
+#define llu long long int unsigned
+#define li long int
+#define ll long long
+#define lli long long int
+//--------------------------
+const double PI=3.141592653589793238462643383279502884197169399375105820974944;
+const int sz = 100005;
+#define INF 1000000000
+#define mod 1000000007
+//--------------------------
+ll modll(ll a){
+
+  a = a%mod;
+  for(ll i=1;i<mod;i++){
+    if((a*i)%mod == 1)
+      return i;
+  }
+}
+
+int main(){
+
+	io1;io2;
+  //cout<<__gcd(-2,mod)<<endl;
+  cout<<(modll(-2))%mod;
+  return 0;
+}
